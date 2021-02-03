8416c81902a389348c5d1649681fe50b1146635a
mbish08 <mdrew8@gmail.com>
2021-02-02 18:18:47 -0800

    adding item files

---
+++ db/migrate/20210203021752_create_items.rb
@@ -0,0 +1,12 @@
+class CreateItems < ActiveRecord::Migration[6.1]
+  def change
+    create_table :items do |t|
+      t.string :name
+      t.string :quantity
+      t.references :user, null: false, foreign_key: true
+      t.references :type, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
