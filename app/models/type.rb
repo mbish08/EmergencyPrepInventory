9f9010f5a790863eedeac8debf550711d2b0723a
mbish08 <mdrew8@gmail.com>
2021-02-02 18:25:58 -0800

    adding relationships to models

--- app/models/type.rb
+++ app/models/type.rb
@@ -1,2 +1,4 @@
 class Type < ApplicationRecord
+    has_many :items
+    has_many :users
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+    has_secure_password
+    has_many :types
+    has_many :items 
 end
