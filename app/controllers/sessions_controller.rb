fedb1a8bd3cdaaefbc1436d80763a83266ef1897
mbish08 <mdrew8@gmail.com>
2021-02-04 20:44:04 -0800

    working on login actions and form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,10 +6,19 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(username: params[:user][:username])
+    if user && user.authenticate(params[:user][:password])
+    session[:user_id] = user.id
+    redirect_to user_path(user)
+    else
+      flash.alert = "Your credentials are invalid, please try again."
+      redirect_to '/login'
+    end
   end
 
   def delete
     session.clear
+    redirect_to '/'
   end
 
 end
