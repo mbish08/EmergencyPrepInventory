772ae8a2be0a06e1e28cf89663b94e6c0eff96ac
mbish08 <mdrew8@gmail.com>
2021-02-04 17:23:55 -0800

    adding pry gem

--- app/models/type.rb
+++ app/models/type.rb
@@ -1,5 +1,4 @@
 class Type < ApplicationRecord
     has_many :items
-    has_many :users
     has_many :users, through: :items
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
     # has_secure_password
-    has_many :types
     has_many :items 
     has_many :types, through: :items
 end
