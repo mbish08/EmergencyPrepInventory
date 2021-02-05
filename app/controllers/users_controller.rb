0c7a94bc3dd6e4e4201839a330a3cd697aa7aa6c
mbish08 <mdrew8@gmail.com>
2021-02-04 17:54:23 -0800

    users/new form drafted

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,17 @@
 class UsersController < ApplicationController
 
     def new
-        @user = User.new(user_params)
+        @user = User.new
+    end
+
+    def create
+        
     end
 
     private
 
     def user_params
-        params.require(:user).permit(:username, :email, :password_digest)
+        params.require(:user).permit(:username, :email, :password)
     end
     
 end
