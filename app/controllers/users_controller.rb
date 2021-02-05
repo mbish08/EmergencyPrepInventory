e7b5c6f149fd8e1160d46f24f382bc343c0f9ba7
mbish08 <mdrew8@gmail.com>
2021-02-04 18:01:58 -0800

    added users#create and users#show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,17 @@ class UsersController < ApplicationController
     end
 
     def create
-        
+      @user = User.new(user_params)
+      if @user.save
+        session[:user_id] = @user.id
+        redirect_to @user
+      else
+        render :new
+      end  
+    end
+
+    def show
+
     end
 
     private
