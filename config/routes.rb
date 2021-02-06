b6cb10c0c84945689ee06a3229773cf059354c61
mbish08 <mdrew8@gmail.com>
2021-02-06 13:13:41 -0800

    updated nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -11,9 +11,9 @@ Rails.application.routes.draw do
 
   resources :users
   resources :items
-  
+
   resources :types do
-    resources :items
+    resources :items, only: [:new, :create, :index]
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
