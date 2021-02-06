066475eea74b3d3612fcca9b9454e3b7d44cf07c
mbish08 <mdrew8@gmail.com>
2021-02-06 09:22:15 -0800

    updated nav bar, added helpers, started items buildout

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,22 @@
 class ItemsController < ApplicationController
+
+    def new
+        @item = Item.new
+    end
+
+    def create
+        @item = current_user.items.build(item_params)
+        if @item.save
+            redirect_to items_path
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def item_params
+        params.require(:item).permit(:title, :content)
+    end
+
 end
