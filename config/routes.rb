0e52df8af14998a070098d901e7babf145d923bf
mbish08 <mdrew8@gmail.com>
2021-02-06 11:30:54 -0800

    start building types

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,10 @@ Rails.application.routes.draw do
 
   resources :users
   resources :items
-  resources :types
+  
+  resources :types do
+    resources :items
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
