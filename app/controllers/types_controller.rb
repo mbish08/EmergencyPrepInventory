410b2fe07333ce306c2cbb14d6052f3fe62b561e
mbish08 <mdrew8@gmail.com>
2021-02-06 18:54:57 -0800

    start building new type form and attributes

--- app/controllers/types_controller.rb
+++ app/controllers/types_controller.rb
@@ -1,5 +1,22 @@
 class TypesController < ApplicationController
 
-   
+   def show
+    @type = Type.find(params[:id])
+   end
+
+   def new
+    @type = Type.new
+    # @type.items.build()
+   end
+
+   def create
+    
+   end
+
+   private
+
+   def type_params
+    params.require(:type).permit(:name, items_attributes: [:name, :quantity])
+   end
 
 end
--- app/models/type.rb
+++ app/models/type.rb
@@ -1,5 +1,15 @@
 class Type < ApplicationRecord
     has_many :items
     has_many :users, through: :items
-   
+    accepts_nested_attributes_for :items
+
+    def items_attributes=(item_attributes)
+        item_attributes.values.each do |item_attribute|
+            if item_attribute["name"].present?
+                item = Item.find_or_create_by(item_attribute)
+                self.items << item
+            end
+        end
+    end
+
 end
