0d11001f36a1f06d08bd28ec186b1fea70eed4f9
mbish08 <mdrew8@gmail.com>
2021-02-07 15:17:17 -0800

    update items/new form and item method

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,10 @@
 class Item < ApplicationRecord
   belongs_to :user
   belongs_to :type
-  accepts_nested_attributes_for :type
-  
+  # accepts_nested_attributes_for :type
+
+  def type_attributes=(attr)
+    self.type = Type.find_or_create_by(name: attr[:name])
+  end
+
 end
