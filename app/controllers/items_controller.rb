8104fe70588c6b031346f157c8577aff0a9ae593
mbish08 <mdrew8@gmail.com>
2021-02-07 18:20:34 -0800

    updated index page and controller action for items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -21,6 +21,11 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def index
+    @user = current_user
+    @items = Item.all
+  end 
+
   private
 
   def item_params
