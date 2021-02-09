bfedbd09486cae01835627126cdba49770827204
mbish08 <mdrew8@gmail.com>
2021-02-08 19:44:15 -0800

    added validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
     has_secure_password
     has_many :items 
     has_many :types, through: :items
+    validates :username, :email, presence: true
+    validates :username, :email, uniqueness: true
 end
