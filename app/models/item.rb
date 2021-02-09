f50f39c2d020a44c5c97c59f195d8cc8e3e8a84f
mbish08 <mdrew8@gmail.com>
2021-02-08 19:25:21 -0800

    added field to items/form partial and validations

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,8 @@ class Item < ApplicationRecord
   belongs_to :user
   belongs_to :type
   # accepts_nested_attributes_for :type
+  validates :name, uniqueness: { case_sensitive: false }
+  validates :name, :quantity, presence: true
 
   def type_attributes=(attr)
     self.type = Type.find_or_create_by(name: attr[:name])
