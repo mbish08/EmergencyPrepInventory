b99a6d19c5f4d03c7632368698a0fe324c55c04a
mbish08 <mdrew8@gmail.com>
2021-02-09 18:30:23 -0800

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   delete '/logout' => 'sessions#delete'
 
   resources :users
-  resources :items
+  resources :items, only: [:show, :edit, :update, :destroy]
 
   resources :types do
     resources :items, only: [:new, :create, :index]
