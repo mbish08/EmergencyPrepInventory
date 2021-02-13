a281bf969a655f61328478011ba0d12a14a4995e
mbish08 <mdrew8@gmail.com>
2021-02-13 15:18:13 -0800

    moved app helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,4 @@
 class ApplicationController < ActionController::Base
-
-    helper_method :current_user, :logged_in?, :redirect_if_not_logged_in
-
-    private
-
-    def current_user
-        @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
-    end
-
-    def logged_in?
-        !!session[:user_id]
-    end
-
-    def redirect_if_not_logged_in
-        redirect_to '/' if !logged_in?
-    end
+    include ApplicationHelper
 
 end
