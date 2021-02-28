709a9c6e0a7e04051077a2fb537caa593b09ccb7
mbish08 <mdrew8@gmail.com>
2021-02-27 18:44:56 -0800

    updated helpers and partials for purchases to not display item fields when item is present

--- app/helpers/purchases_helper.rb
+++ app/helpers/purchases_helper.rb
@@ -8,12 +8,13 @@ module PurchasesHelper
         end
     end
 
-    # def display_item_fields(t)
-    #     if params[:item_id]
-    #         t.hidden_field :item_id
-    #     else
-    #         render partial: 'item_select', locals: {f: t}
-    #     end
-    # end
+    # used for purchases/new
+    def display_item_fields(t)
+        if params[:item_id]
+            t.hidden_field :item_id
+        else
+            render partial: 'item_select', locals: {f: t}
+        end
+    end
 
 end
