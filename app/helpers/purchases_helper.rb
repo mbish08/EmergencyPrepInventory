4a53225a83b49369dbeea939cd8eae7042953bae
mbish08 <mdrew8@gmail.com>
2021-03-05 10:45:14 -0800

    partially working order_by_count in view

--- app/helpers/purchases_helper.rb
+++ app/helpers/purchases_helper.rb
@@ -9,4 +9,14 @@ module PurchasesHelper
         end
     end
 
+    # used in popular view
+    def display_top_three(purchases)
+        purchases.each do | item, count | 
+            item_find = Item.find_by(id: item)
+            @item_name = item_find.name
+            @item_count = count
+            # byebug
+        end
+    end
+
 end
