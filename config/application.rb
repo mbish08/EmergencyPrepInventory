0c0a1736b23651bdde73a66ad9045fb062f8c033
mbish08 <mdrew8@gmail.com>
2021-09-01 11:25:19 -0700

    cleanup unused code

--- config/application.rb
+++ config/application.rb
@@ -10,13 +10,6 @@ module EmergencyPrepInventory
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
   end
+  
 end
